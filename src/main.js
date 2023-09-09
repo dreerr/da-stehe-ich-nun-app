a31635c66b7ba63be6c9ec5b8916d0a7352eac7e
Julian Palacz <julian.palacz@nhm-wien.ac.at>
2023-09-09 14:39:14 +0200

    feat: analytics, opengraph

--- src/main.js
+++ src/main.js
@@ -4,12 +4,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
+import VueMatomo from 'vue-matomo'
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(router)
 app.use(pinia)
+app.use(VueMatomo, {
+  host: 'https://palacz.at/analytics/',
+  siteId: 5,
+  router: router
+})
 
 router.isReady().then(() => {
   app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,42 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vitePluginFaviconsInject from 'vite-plugin-favicons-inject'
+import ogPlugin from 'vite-plugin-open-graph'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vitePluginFaviconsInject('./src/assets/logo.png')],
+  plugins: [
+    vue(),
+    vitePluginFaviconsInject('./src/assets/logo.png', {
+      appName: 'Da stehe ich nun...',
+      appDescription:
+        'Die geballte Präsenz eines unermüdlichen Detektivs, dessen Monolog sich wie ein spannungsgeladener Krimi durch die Straßen und Plätze von Wien windet',
+      developerName: 'Julian Palacz',
+      developerURL: 'https://julian.palacz.at',
+      lang: 'de-AT'
+    }),
+    ogPlugin({
+      basic: {
+        url: 'https://da-stehe-ich-nun.palacz.at',
+        title: 'Da stehe ich nun...',
+        image: 'https://da-stehe-ich-nun.palacz.at/website.jpg',
+        type: 'Website',
+        determiner: 'auto',
+        description:
+          'Die geballte Präsenz eines unermüdlichen Detektivs, dessen Monolog sich wie ein spannungsgeladener Krimi durch die Straßen und Plätze von Wien windet',
+        locale: 'de_AT',
+        siteName: 'Da stehe ich nun...'
+      },
+      twitter: {
+        title: 'Da stehe ich nun...',
+        image: 'https://da-stehe-ich-nun.palacz.at/website.jpg',
+        description:
+          'Die geballte Präsenz eines unermüdlichen Detektivs, dessen Monolog sich wie ein spannungsgeladener Krimi durch die Straßen und Plätze von Wien windet'
+      }
+    })
+  ],
+  assetsInclude: ['@/assets/hero.jpg'],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
